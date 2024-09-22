e0788561c64c5086d84a26795f640dde66ca99d6
Luka Weis <lukat.weis@gmail.com>
2024-09-22 23:56:28 +0200

    fix(Notifications): Add bottom margin to declutter notifications

--- kaeltebus-frontend/src/common/utils/http.tsx
+++ kaeltebus-frontend/src/common/utils/http.tsx
@@ -14,6 +14,7 @@ const notificationProps: Partial<NotificationData> = {
   icon: <IconX />,
   withBorder: false,
   withCloseButton: true,
+  mb: "xs",
 };
 http.interceptors.response.use(
   (response) => response,
