a5efe00fc0a5ff002269b1e2dc145e6a4dbfc324
Luka Weis <lukat.weis@gmail.com>
2026-01-18 22:01:25 +0100

    feature: Rebrand heading

--- kaeltehilfe-frontend/src/AppShell.tsx
+++ kaeltehilfe-frontend/src/AppShell.tsx
@@ -7,55 +7,65 @@ import {
 import { useDisclosure, useViewportSize } from "@mantine/hooks";
 import React from "react";
 import { Outlet } from "react-router-dom";
-import LogoLight from "./common/assets/drk_logo.png";
-import LogoDark from "./common/assets/drk_logo_dark.png";
 import { UserMenu } from "./common/components";
+
+const cupDark = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+    >
+      <g
+        fill="none"
+        stroke="#000000"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1.5"
+      >
+        <path d="M17 11.6V15a6 6 0 0 1-6 6H9a6 6 0 0 1-6-6v-3.4a.6.6 0 0 1 .6-.6h12.8a.6.6 0 0 1 .6.6M12 9c0-1 .714-2 2.143-2v0A2.857 2.857 0 0 0 17 4.143V3.5M8 9v-.5a3 3 0 0 1 3-3v0a2 2 0 0 0 2-2V3" />
+        <path d="M16 11h2.5a2.5 2.5 0 0 1 0 5H17" />
+      </g>
+    </svg>
+  );
+};
+const cupLight = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+    >
+      <g
+        fill="none"
+        stroke="#ffffff"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1.5"
+      >
+        <path d="M17 11.6V15a6 6 0 0 1-6 6H9a6 6 0 0 1-6-6v-3.4a.6.6 0 0 1 .6-.6h12.8a.6.6 0 0 1 .6.6M12 9c0-1 .714-2 2.143-2v0A2.857 2.857 0 0 0 17 4.143V3.5M8 9v-.5a3 3 0 0 1 3-3v0a2 2 0 0 0 2-2V3" />
+        <path d="M16 11h2.5a2.5 2.5 0 0 1 0 5H17" />
+      </g>
+    </svg>
+  );
+};
+export const Logo = () => {
+  const { colorScheme } = useMantineColorScheme();
+  return (
+    <div className="brand">
+      {colorScheme === "dark" ? cupLight() : cupDark()}
+      <span className="company">Kältehilfe</span>
+    </div>
+  );
+};
 
 export const AppShell = ({ navigation }: { navigation?: React.ReactNode }) => {
   const [opened, { toggle }] = useDisclosure();
 
-  const { colorScheme } = useMantineColorScheme();
-
   const { width } = useViewportSize();
   const isSmall = width < 450;
-
-  // const lastLocationState = React.useState<string>();
-
-  // Add required html tags for using the map view
-  React.useEffect(() => {
-    // const { head } = document;
-    // const existingLeafletStylesheet = head.querySelector(
-    //   'link[rel="stylesheet"][href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"]'
-    // );
-    // if (!existingLeafletStylesheet) {
-    //   const leafletStylesheet = document.createElement("link");
-    //   leafletStylesheet.rel = "stylesheet";
-    //   leafletStylesheet.href =
-    //     "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css";
-    //   leafletStylesheet.integrity =
-    //     "sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=";
-    //   leafletStylesheet.crossOrigin = "";
-    //   head.appendChild(leafletStylesheet);
-    // }
-    // const existingLeafletScript = head.querySelector(
-    //   'script[src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"]'
-    // );
-    // if (!existingLeafletScript) {
-    //   const leafletScript = document.createElement("script");
-    //   leafletScript.src = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";
-    //   leafletScript.integrity =
-    //     "sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=";
-    //   leafletScript.crossOrigin = "";
-    //   head.appendChild(leafletScript);
-    // }
-    // const existingMapContainer = document.getElementById("map");
-    // if (!existingMapContainer) {
-    //   const mapContainer = document.createElement("div");
-    //   mapContainer.id = "map";
-    //   mapContainer.style.height = "500px";
-    //   document.body.appendChild(mapContainer);
-    // }
-  }, []);
 
   return (
     <Mantine_AppShell
@@ -74,14 +84,12 @@ export const AppShell = ({ navigation }: { navigation?: React.ReactNode }) => {
           px="md"
           justify="space-between"
         >
-          <Group className="BurgerMenu">
+          <Group className="burger-menu">
             <Burger opened={opened} onClick={toggle} size="sm" />
-            <img
-              height={80}
-              src={colorScheme === "dark" ? LogoDark : LogoLight}
-            />
+
+            <Logo />
           </Group>
-          <Group className="UserMenu">
+          <Group className="user-menu">
             <UserMenu />
           </Group>
         </Group>
