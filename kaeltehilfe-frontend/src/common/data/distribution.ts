0305796e6c57a12c5b167492e463611f5a1b7f54
Luka Weis <lukat.weis@gmail.com>
2026-04-20 13:53:40 +0200

    fix: Transform distribution dates; Do not use paginated distribution query for two week threshold warnings

--- kaeltehilfe-frontend/src/common/data/distribution.ts
+++ kaeltehilfe-frontend/src/common/data/distribution.ts
@@ -4,11 +4,7 @@ import {
   useQueryClient,
 } from "@tanstack/react-query";
 import React from "react";
-import {
-  getBaseQuery,
-  toNormalizedDate,
-  useCrudHook,
-} from "../utils";
+import { getBaseQuery, toDate, toNormalizedDate, useCrudHook } from "../utils";
 import { GeoLocation } from "./geoLocation";
 
 export type Distribution = {
@@ -49,70 +45,7 @@ export const useDistributions = (params: DistributionsQueryParams) =>
   >({
     key: "distributions",
     params,
+    transformer: { timestamp: (value) => toDate(value)! },
     additionalInvalidation: ["distributionsPaginated"],
     enabled: !!params?.from && !!params?.to,
   });
-
-export const useDistributionsPaginated = () => {
-  const httpGet = getBaseQuery<Distribution>(`/distributions`);
-
-  const queryClient = useQueryClient();
-
-  // Track empty results to stop pagination
-  const emptyPageCountRef = React.useRef(0);
-
-  const now = new Date();
-  const oneHourAhead = new Date(now.setHours(now.getHours() + 1));
-  const oneMonthBefore = toNormalizedDate(
-    new Date(new Date(now).setMonth(now.getMonth() - 1).valueOf()),
-  )!;
-
-  const queryDistributionsPaginated = useInfiniteQuery<
-    Distribution[],
-    Error,
-    InfiniteData<Distribution>,
-    ["distributionsPaginated"],
-    { from: Date; to: Date }
-  >(
-    {
-      queryKey: ["distributionsPaginated"],
-      queryFn: (params) => httpGet(params.signal, params.pageParam),
-      initialPageParam: {
-        from: oneMonthBefore,
-        to: oneHourAhead,
-      },
-      getNextPageParam: (lastPage, _allPages, lastPageParams) => {
-        // If last page returned no data, increase the emptyPageCountRef counter
-        if (!lastPage || lastPage?.length === 0) {
-          emptyPageCountRef.current++;
-        } else {
-          emptyPageCountRef.current = 0; // Reset if we have data
-        }
-
-        // Stop fetching if the last 3 pages yielded no results
-        if (emptyPageCountRef.current >= 3) {
-          return undefined; // Return undefined to stop fetching more pages
-        }
-
-        // Calculate the next pages "from" and "to" date range
-        const to = lastPageParams.from;
-        const from = new Date(
-          new Date(to).setMonth(to.getMonth() - 1).valueOf(),
-        );
-        return {
-          from,
-          to,
-        };
-      },
-    },
-    queryClient,
-  );
-
-  const invalidate = () => {
-    queryClient.invalidateQueries({
-      queryKey: ["distributionsPaginated"],
-    });
-  };
-
-  return { queryDistributionsPaginated, invalidate };
-};
